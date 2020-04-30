9b620760ecd545840a06a5ea89874ff5aed6362a
zhao ziyi <zhaoziy@oregonstate.edu>
2020-04-30 15:59:40 -0700

    connected database

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req, res) => res.send('API Runing'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
