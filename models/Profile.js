3d92ca134531c56d90fecb0c135c014b28f72e1d
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-02 17:15:30 -0700

    add user profile

---
+++ models/Profile.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company:{
+        type:String
+    },
+    about:{
+        type:String
+    },
+    website:{
+        type:String,
+    },
+    location:{
+        type:String
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    delivery:{
+        type:String
+    },
+    takeout:{
+        type:String
+    },
+    contact:{
+        type:Number
+    },
+    social:{
+        twitter:{
+            type:String
+        },
+        facebook:{
+            type:String
+        },
+        instagram:{
+            type:String
+        }
+    },
+    date: {
+        type:Date,
+        default:Date.now
+    }
+
+
+});
+
+module.exports = Profile = mongoose.model('profile',ProfileSchema);
