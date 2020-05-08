d34f53debdb316422f58dfcc65912173e192c504
Angela Venger <venger@Angelas-MacBook-Pro.local>
2020-05-07 21:33:09 -0700

    add like & unlike functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -47,6 +48,7 @@ const App = () =>{
             <PrivateRoute exact path='/dashboard' component={ Dashboard } />
             <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
             <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
+            <PrivateRoute exact path='/posts' component={ Posts } />
           </Switch>
         </section>
       </Fragment>
---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,78 @@
+import React, { Fragment } from 'react';
+import  Moment  from 'react-moment';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
+
+const PostItem = ({ 
+  addLike,
+  removeLike,
+  auth, 
+  post: {
+  _id,
+  text,
+  name,
+  avatar,
+  user,
+  likes,
+  comments,
+  date
+}}) => (
+    <div className="post bg-white p-1 my-1">
+    <div>
+      <a href="profile.html">
+        <img
+          className="round-img"
+          src={avatar}
+          alt=""
+        />
+        <h4>{name}</h4>
+      </a>
+    </div>
+    <div>
+      <p className="my-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+        possimus corporis sunt necessitatibus! Minus nesciunt soluta
+        suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
+        dolor? Illo perferendis eveniet cum cupiditate aliquam?
+      </p>
+       <p className="post-date">
+          Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+      </p>
+      <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+        <i className="fas fa-thumbs-up"></i>
+        <span> {likes.length > 0 && (
+           <span> {likes.length} </span>
+        )} </span>
+      </button>
+      <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+        <i className="fas fa-thumbs-down"></i>
+      </button>
+      <Link to={`/post/${_id}`} className="btn btn-primary">
+        Discussion {comments.length > 0 && (
+          <span className='comment-count'> {comments.length} </span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+
+        <button type="button" className="btn btn-danger">
+          <i className="fas fa-times"></i>
+        </button>
+        )}
+    </div>
+  </div>
+
+  )
+
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth:PropTypes.object.isRequired
+};
+
+
+const mapStateToProps = state => ({
+  auth:state.auth
+})
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
